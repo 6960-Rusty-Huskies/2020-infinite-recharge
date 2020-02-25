005c5379d5be014cf13ce4f3ee0e7968e7193c03
Valencia <stu427409@rusd.edu>
2020-02-24 19:03:40 -0800

    This is what i tried to do with the repeating patterns, Brannon

--- src/main/java/com/north6960/lights/AlternatingPattern.java
+++ src/main/java/com/north6960/lights/AlternatingPattern.java
@@ -31,17 +31,17 @@ public class AlternatingPattern extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    // m_rgb.strip.shiftPatternUp();
+     m_rgb.strip.shiftPatternUp();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
   }
-
   // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
+  /*@Override
+   public boolean isFinished() {
     return false;
   }
+*/
 }
